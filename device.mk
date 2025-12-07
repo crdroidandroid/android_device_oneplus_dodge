bde3bb0373d1e65162a463e2a4c8bd73b299d95c
dianlujitao <dianlujitao@lineageos.org>
2025-12-07 22:04:42 +0000

    dodge: Modified .bin files to improve haptic feedback.
    Inspired by commit one, building upon it with commit two.
    
    dodge: Double the amplitude of click vibration
    
    Users keep complaining that the current click is too light.
    
    Produced by:
    ```
    import numpy as np
    bin_path = 'effect_0.bin'
    data = np.fromfile(bin_path, dtype=np.int8)
    new = data * 2
    new.tofile(bin_path)
    ```
    
    Change-Id: Ibe327124b39c17865dc6c2584df6717f0ae22587
    
    dodge: Modified .bin files to improve haptic feedback.
    
    After testing, it was found that AOSP's vibration system invokes effect .bin files differently from OOS / COS, resulting in different haptic feedback even when using the same .bin files. To address this, the original OEM .bin files have been renamed to align with AOSP's invocation mechanism, making the final vibration experience much closer to that of OOS/COS.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -89,6 +89,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/vibrator/def/effect_0.bin:$(TARGET_COPY_OUT_ODM)/etc/vibrator/9999/def/effect_0.bin\
+    $(LOCAL_PATH)/configs/vibrator/def/effect_1.bin:$(TARGET_COPY_OUT_ODM)/etc/vibrator/9999/def/effect_1.bin\
+    $(LOCAL_PATH)/configs/vibrator/def/effect_2.bin:$(TARGET_COPY_OUT_ODM)/etc/vibrator/9999/def/effect_2.bin\
+    $(LOCAL_PATH)/configs/vibrator/def/effect_21.bin:$(TARGET_COPY_OUT_ODM)/etc/vibrator/9999/def/effect_21.bin
+
 $(call soong_config_set_bool,OPLUS_LINEAGE_VIBRATOR_HAL,USE_EFFECT_STREAM,true)
 $(call soong_config_set,OPLUS_LINEAGE_VIBRATOR_HAL,INCLUDE_DIR,$(LOCAL_PATH)/vibrator/include)
 
